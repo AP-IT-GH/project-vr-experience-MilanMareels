26efefeafc7b01b3a8f99853a96d5cd2e26ba440
MilanMareels <milan.mareels@gmail.com>
2025-05-27 14:12:22 +0200

    added box for ball resert vr player and agent(t

---
+++ World-Petanque/Assets/Scripts/BallResetZone.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using UnityEngine;
+
+public class BallResetZone : MonoBehaviour
+{
+    public GameObject ball; // Assign this in the Inspector
+
+    private Vector3 originalPosition;
+    private Quaternion originalRotation;
+    private Rigidbody ballRb;
+    private Coroutine resetCoroutine;
+
+    void Start()
+    {
+        if (ball != null)
+        {
+            originalPosition = ball.transform.position;
+            originalRotation = ball.transform.rotation;
+            ballRb = ball.GetComponent<Rigidbody>();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == ball)
+        {
+            // Start waiting to reset
+            if (resetCoroutine == null)
+                resetCoroutine = StartCoroutine(WaitAndReset());
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == ball)
+        {
+            // Cancel the reset if the ball leaves early
+            if (resetCoroutine != null)
+            {
+                StopCoroutine(resetCoroutine);
+                resetCoroutine = null;
+            }
+        }
+    }
+
+    IEnumerator WaitAndReset()
+    {
+        yield return new WaitForSeconds(5f);
+
+        // Reset position, rotation and velocity
+        ball.transform.position = originalPosition;
+        ball.transform.rotation = originalRotation;
+
+        if (ballRb != null)
+        {
+            ballRb.linearVelocity = Vector3.zero;
+            ballRb.angularVelocity = Vector3.zero;
+        }
+
+        resetCoroutine = null;
+    }
+}
