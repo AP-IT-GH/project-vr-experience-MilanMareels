672c5c1dab0561464dbfaaa75f5de6d86d2d9240
MilanMareels <milan.mareels@gmail.com>
2025-05-27 14:39:18 +0200

    fix agent thow reset

--- World-Petanque/Assets/Scripts/BallResetZone.cs
+++ World-Petanque/Assets/Scripts/BallResetZone.cs
@@ -1,64 +1,86 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class BallResetZone : MonoBehaviour
 {
-    public GameObject ball;
+    [Header("Assign Both Balls")]
+    public GameObject playerBall;
+    public GameObject agentBall;
 
     public int resetTime;
 
-    private Vector3 originalPosition;
-    private Quaternion originalRotation;
-    private Rigidbody ballRb;
-    private Coroutine resetCoroutine;
+    private Dictionary<GameObject, BallData> ballDataMap = new Dictionary<GameObject, BallData>();
 
     void Start()
     {
+        SetupBall(playerBall);
+        SetupBall(agentBall);
+    }
+
+    void SetupBall(GameObject ball)
+    {
         if (ball != null)
         {
-            originalPosition = ball.transform.position;
-            originalRotation = ball.transform.rotation;
-            ballRb = ball.GetComponent<Rigidbody>();
+            BallData data = new BallData
+            {
+                originalPosition = ball.transform.position,
+                originalRotation = ball.transform.rotation,
+                rb = ball.GetComponent<Rigidbody>()
+            };
+
+            ballDataMap[ball] = data;
         }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject == ball)
+        if (ballDataMap.ContainsKey(other.gameObject))
         {
-            // Start waiting to reset
-            if (resetCoroutine == null)
-                resetCoroutine = StartCoroutine(WaitAndReset());
+            BallData data = ballDataMap[other.gameObject];
+
+            if (data.resetCoroutine == null)
+                data.resetCoroutine = StartCoroutine(WaitAndReset(other.gameObject));
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.gameObject == ball)
+        if (ballDataMap.ContainsKey(other.gameObject))
         {
-            // Cancel the reset if the ball leaves early
-            if (resetCoroutine != null)
+            BallData data = ballDataMap[other.gameObject];
+
+            if (data.resetCoroutine != null)
             {
-                StopCoroutine(resetCoroutine);
-                resetCoroutine = null;
+                StopCoroutine(data.resetCoroutine);
+                data.resetCoroutine = null;
             }
         }
     }
 
-    IEnumerator WaitAndReset()
+    IEnumerator WaitAndReset(GameObject ball)
     {
         yield return new WaitForSeconds(resetTime);
 
-        // Reset position, rotation and velocity
-        ball.transform.position = originalPosition;
-        ball.transform.rotation = originalRotation;
+        BallData data = ballDataMap[ball];
 
-        if (ballRb != null)
+        ball.transform.position = data.originalPosition;
+        ball.transform.rotation = data.originalRotation;
+
+        if (data.rb != null)
         {
-            ballRb.linearVelocity = Vector3.zero;
-            ballRb.angularVelocity = Vector3.zero;
+            data.rb.linearVelocity = Vector3.zero;
+            data.rb.angularVelocity = Vector3.zero;
         }
 
-        resetCoroutine = null;
+        data.resetCoroutine = null;
+    }
+
+    class BallData
+    {
+        public Vector3 originalPosition;
+        public Quaternion originalRotation;
+        public Rigidbody rb;
+        public Coroutine resetCoroutine;
     }
 }
